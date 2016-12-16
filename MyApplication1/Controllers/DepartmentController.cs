dcc05446e5d9c86116fd4b103f3b7ac9b1f02b78
balwanth1993 <balwanth.r93@gmail.com>
2016-12-16 14:30:38 -0600

    Add project files.

---
+++ MyApplication1/Controllers/DepartmentController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyApplication1.Models;
+
+namespace MyApplication1.Controllers
+{
+    public class DepartmentController : Controller
+    {
+        
+        public ActionResult Index()
+        {
+            StudentDBContext studentDBContext = new StudentDBContext();
+            List<Department> departments = studentDBContext.Departments.ToList();
+            return View(departments);
+        }
+    }
+}
---
+++ MyApplication1/Controllers/HomeController.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MyApplication1.Models;
+
+namespace MyApplication1.Controllers
+{
+    public class HomeController : Controller
+    {
+        
+        public ActionResult Index(int Id)
+        {
+            StudentDBContext studentDBContext = new StudentDBContext();
+            List<Student> students= studentDBContext.Students.Where(stu => stu.DepartmentId == Id).ToList();
+            return View(students);
+
+        }
+        public ActionResult Details(int Id)
+        {
+            StudentDBContext studentDBContext = new StudentDBContext();
+            Student students = studentDBContext.Students.Single(x =>x.ID == Id);
+            return View(students);
+
+        }
+        public ActionResult About()
+        {
+            return RedirectToAction("Index", "Department",null);
+        }
+
+
+    }
+}
